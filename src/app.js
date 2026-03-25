d9f432318bde638af84a36da33209d528b2ca1c1
Darshith1316 <darshithcm@gmail.com>
2026-03-25 10:57:13 +0530

    Example 1

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const todoRoutes = require('./routes/todos');
 
 const app = express();
@@ -7,6 +8,7 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../public')));
 
 // Routes
 app.get('/health', (req, res) => {
